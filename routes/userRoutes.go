94b1d1f7c094d6c2af78f29ab6edfd0e00379677
Muhamed Jasim <jasimmtp84@gmail.com>
2023-11-15 23:42:23 +0530

    updated

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -8,9 +8,9 @@ import (
 func UserRoutes(r *gin.Engine) {
 
 	r.POST("/signup", handlers.UserSignup)
-	r.POST("/verify", handlers.VerifySignupOtp)
+	// r.POST("/verify", handlers.VerifySignupOtp)
 	r.POST("/login", handlers.UserLogin)
-	r.POST("/login/verify", handlers.VerifyLoginOtp)
+	r.POST("/verify", handlers.VerifyLoginOtp)
 	r.POST("/logout", handlers.UserLogout)
 	// r.POST("/products"handlers.ShowAllProducts)
 
