6945a2419b27d47be599d1a3f77c73bc3284bf94
Muhamed Jasim <jasimmtp84@gmail.com>
2023-11-28 16:32:59 +0530

    updated middleware

--- middlewares/user.go
+++ middlewares/user.go
@@ -10,6 +10,7 @@ import (
 func UserAuthMiddleware(c *gin.Context) {
 	Token, err := c.Cookie("Authorisation")
 	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "login to view page"})
 		c.AbortWithStatus(http.StatusUnauthorized)
 	}
 	role, err := utils.GetRoleFromToken(Token)
