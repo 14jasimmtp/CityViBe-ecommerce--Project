ccd4dedab168d7ffaf79308436735f014d63a880
Muhamed Jasim <jasimmtp84@gmail.com>
2023-12-18 13:55:46 +0530

    configured env

--- Initialisers/LoadEnvVariables.go
+++ Initialisers/LoadEnvVariables.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load("/home/ubuntu/CityViBe-Project-Ecommerce/.env")
+	err := godotenv.Load("/home/ubuntu/CityViBe-ecommerce--Project/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file", err)
 	}
